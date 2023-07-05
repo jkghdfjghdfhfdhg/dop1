85f7773fd00066aaedaf9ec8be0f33a19bb905d1
jkghdfjgdfhfdhg <kurbanov.rustam.mansurovich@gmail.com>
2023-07-05 19:02:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState([]);
+  const [value, setValue] = useState("");
+  const [desc, setDesc] = useState("");
+
+  const onAddTask = () => {
+    const task = {
+      text: value,
+      description: desc,
+      completed: false
+    };
+    setState([...state, task]);
+    console.log(state);
+  };
+
+  const onDelete = (item) => {
+    setState(state.filter((el) => el.text !== item.text));
+    console.log(state);
+  };
+
+  const onToggleComplete = (item) => {
+    setState(state.map((el) => {
+      if (el.text === item.text) {
+        return {
+          ...el,
+          completed: !el.completed
+        };
+      }
+      return el;
+    }));
+  };
+
+  const onEdit = (item, newText) => {
+    setState(state.map((el) => {
+      if (el.text === item.text) {
+        return {
+          ...el,
+          text: newText
+        };
+      }
+      return el;
+    }));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          type="title"
+        />
+        <input
+          value={desc}
+          onChange={(e) => {
+            setDesc(e.target.value);
+          }}
+          type="text"
+        />
+      </div>
+      <input onClick={onAddTask} type="submit" />
+      <div>
+        {state.map((item, i) => {
+          return (
+            <div key={i}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={item.completed}
+                  onChange={() => onToggleComplete(item)}
+                />
+                <span style={{ textDecoration: item.completed ? "line-through" : "none" }}>
+                  {item.text}
+                </span>
+              </div>
+              {item.completed ? null : (
+                <div>
+                  <input
+                    type="submit"
+                    value="Edit"
+                    onClick={() => {
+                      const newText = prompt("Enter new task");
+                      onEdit(item, newText);
+                    }}
+                  />
+                </div>
+              )}
+              <input onClick={() => onDelete(item)} type="submit" value={"delete"} />
+              <input type="submit" value="important" />
+              <input type="submit" value="done" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
 
 export default App;
+
